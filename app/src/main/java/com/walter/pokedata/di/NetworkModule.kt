4d75301f59bd3692185b20ff36b2b8b1e6771eab
Walter <walter_rfl@hotmail.com>
2021-01-27 00:36:39 -0300

    updated hilt version;

--- app/src/main/java/com/walter/pokedata/di/NetworkModule.kt
+++ app/src/main/java/com/walter/pokedata/di/NetworkModule.kt
@@ -4,14 +4,14 @@ import com.walter.pokedata.PokemonService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     @Singleton
