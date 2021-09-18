f21e73d5164b3c74d6d708d87dd537f7c7ef184c
Walter <walter_rfl@hotmail.com>
2021-09-18 16:41:48 -0300

    Init modules; created login and buildSrc modules

---
+++ buildSrc/src/main/kotlin/version/Hilt.kt
@@ -0,0 +1,18 @@
+package version
+
+object Hilt {
+    private const val daggerHiltVersion = "2.31.2-alpha"
+    private const val hiltVersion = "1.0.0"
+
+    const val daggerHiltAndroid = "com.google.dagger:hilt-android:$daggerHiltVersion"
+    const val common = "androidx.hilt:hilt-common:$hiltVersion"
+    const val hiltViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
+
+    const val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:$daggerHiltVersion"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:$hiltVersion"
+
+    object Test {
+        const val androidTesting = "com.google.dagger:hilt-android-testing:$daggerHiltVersion"
+        const val daggerHiltCompiler = "com.google.dagger:hilt-compiler:$daggerHiltVersion"
+    }
+}
