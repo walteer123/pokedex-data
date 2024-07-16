38d20c87fed23768ad6b7e505ef22675ac8a8cf6
Walter Alves <walter_rfl@hotmail.com>
2024-07-16 02:01:59 -0300

    adjust remote data and service impl

--- home/src/main/java/com/walter/pokedata/home/data/extensions/DataMappers.kt
+++ home/src/main/java/com/walter/pokedata/home/data/extensions/DataMappers.kt
@@ -2,13 +2,9 @@ package com.walter.pokedata.home.data.extensions
 
 import com.walter.pokedata.home.domain.entity.Pokemon
 import com.walter.pokedata.shared.data.entity.PokemonRemote
+import com.walter.pokedata.shared.data.extensions.getImageUrl
 
 fun PokemonRemote.transform() = Pokemon(
     name = name,
     imageUrl = getImageUrl()
 )
-
-private fun PokemonRemote.getImageUrl(): String {
-    val index = url?.split("/".toRegex())?.dropLast(1)?.last()
-    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png"
-}
--- shared/src/commonMain/kotlin/com/walter/pokedata/shared/data/service/PokemonServiceImpl.kt
+++ shared/src/commonMain/kotlin/com/walter/pokedata/shared/data/service/PokemonServiceImpl.kt
@@ -4,15 +4,15 @@ import com.walter.pokedata.shared.data.entity.PokemonListResponse
 import com.walter.pokedata.shared.network.factory.NetworkServiceFactory
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.statement.request
 import io.ktor.http.parameters
 
 class PokemonServiceImpl(private val serviceFactory: NetworkServiceFactory): PokemonService {
     override suspend fun fetchPokemonList(limit: Int, offset: Int): PokemonListResponse {
-         val teste = serviceFactory.client().get("pokemon"){
-            parameters {
-                append("limit", limit.toString())
-                append("offset", offset.toString())
-            }
+         val teste = serviceFactory.client().get("pokemon/"){
+             parameter("limit", limit)
+             parameter("offset", offset)
         }
         return teste.body()
     }
